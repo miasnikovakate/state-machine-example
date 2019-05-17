ca85c22a70455eb130140aa3a745ce6f0f6be299
Ekaterina_Miasnikova <Ekaterina_Miasnikova@epam.com>
2019-05-17 18:05:28 +0400

    small fix

--- src/main/java/com/epam/sm/example/model/Order.java
+++ src/main/java/com/epam/sm/example/model/Order.java
@@ -2,7 +2,15 @@ package com.epam.sm.example.model;
 
 import lombok.Data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Data
 public class Order {
     private DeliveryType deliveryType;
+    private Map<String, Object> things = new HashMap<>();
+
+    public void addThing(String name, Object thing) {
+        things.put(name, thing);
+    }
 }
